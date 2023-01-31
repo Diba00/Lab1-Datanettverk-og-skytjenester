fc1d2362cfa15f7db9ab5ff9a34ed84045ed0b92
Diba00 <dibas.shishegar@gmail.com>
2023-01-31 17:22:53 +0100

    Kommentarer til koden

--- Lab1.py
+++ Lab1.py
@@ -1,7 +1,6 @@
 def jains(x1,x2):
-  N = 2
-  return ((x1 + x2)**2) / (N * (x1**2 + x2**2))
+  N = 2 # N er 2 fordi vi har 2 input variabler
+  return ((x1 + x2)**2) / (N * (x1**2 + x2**2)) # Dette er JFI formelen vi fikk
 
-result = jains(5,2)
-print(result)
-
+result = jains(1,2) #Resultatet med to tall jeg har valgt
+print("the sum is" , result) #Summen
